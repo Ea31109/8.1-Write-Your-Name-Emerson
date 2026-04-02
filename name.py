621d8f69444abe7d4c351e4fea3729cf3cba5bff
Your Name <ea160048@gmail.com>
2026-04-02 16:32:23 -0500

    first

---
+++ name.py
@@ -0,0 +1,13 @@
+from turtle import*
+
+screen = Screen()
+screen.title("white your name")
+screen.bgcolor("black")
+screen.setup(width = 1000, height = 400)
+yurtle = Turtle()
+yurtle.color("green")
+yurtle.shape("turtle")
+
+
+
+screen.exitonclick()
